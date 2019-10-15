5b14610f5040c2c8ed4eacfe95a77fba97b6b043
Your Name <you@example.com>
2019-10-15 10:37:58 +0200

    füge vorname und nachname in über JPA in Datenbank

--- src/main/java/com/example/entity/Customer.java
+++ src/main/java/com/example/entity/Customer.java
@@ -14,6 +14,34 @@ public class Customer
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
+  private String vorname;
+
+  private String nachname;
+
+
+  public String getVorname()
+  {
+    return vorname;
+  }
+
+
+  public void setVorname(String vorname)
+  {
+    this.vorname = vorname;
+  }
+
+
+  public String getNachname()
+  {
+    return nachname;
+  }
+
+
+  public void setNachname(String nachname)
+  {
+    this.nachname = nachname;
+  }
+
   @Override
   public String toString()
   {
